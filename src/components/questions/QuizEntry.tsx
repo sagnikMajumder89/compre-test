d698cef24ee0454c4c18d9f288b237b48197834d
Sagnik Majumder <sagnikm183@gmail.com>
2025-10-01 18:44:49 +0530

    update

--- src/components/questions/QuizEntry.tsx
+++ src/components/questions/QuizEntry.tsx
@@ -27,6 +27,7 @@ const quizSets = [
   { label: "Set 20", file: "/assets/questions20.json" },
   { label: "Set 21", file: "/assets/questions21.json" },
   { label: "Set 22", file: "/assets/questions22.json" },
+  { label: "Set 23", file: "/assets/questions23.json" },
 ];
 
 export const QuizEntry: React.FC = () => {
